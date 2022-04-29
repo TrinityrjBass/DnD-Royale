fe41597959f5f6d23536a57e3eea67f6d847179a
Trinity Bass <trintiyrjbass@gmail.com>
2022-04-28 20:05:20 -0500

    troubleshooting production env pt 3

--- newFlask/DnDRoyale/__init__.py
+++ newFlask/DnDRoyale/__init__.py
@@ -6,13 +6,12 @@ from distutils.log import debug
 from flask import Flask
 import os #temp debugging
 
-breadcrumb = os.listdir()
+breadcrumb = os.getcwd()
 print("initializing __init__.py from " + str(breadcrumb))
 
 app = Flask(__name__)
 print("got to init, app created")
-app.config["DEBUG"] = True
+app.config["DEBUG"] = False #False for Production
 import DnDRoyale.views
-#print("just finished importing views.py") #for debugging
 
 
